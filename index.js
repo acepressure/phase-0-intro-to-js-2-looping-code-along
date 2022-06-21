3827533c2259853a908add4c7adb3aa2003dd2ef
Walter Simon <simonwalter986@gmail.com>
2022-06-21 12:16:08 +0300

    commit tree a999ee7508edf5b2ed74124fb89b3cf8af733f48

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards( names, action ) {
+    let thankYou = []
+    for ( let i = 0; i < names.length; i++ ) {
+      thankYou.push( `Thank you, ${names[i]}, for the wonderful ${action} gift!` );
+    }
+    return thankYou;
+  }
+
+  function countDown(number) {
+    while (number > 0 ) {
+      console.log(number);
+      number -= 1;
+    }
+    console.log(number);
+  }
